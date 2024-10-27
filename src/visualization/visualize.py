7ba070ff59661efe61b2e0728f1bf9661402660d
Yashs Tiwari <yashstiwari03@gmail.com>
2024-10-27 23:09:29 +0530

    updated visualization code

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -87,12 +87,88 @@ plt.legend()
 # Create a loop to plot all combinations per sensor
 # --------------------------------------------------------------
 
+labels = df["label"].unique()
+participants = df["participant"].unique()
+
+for label in labels:
+    for participant in participants:
+        all_axis_df = (
+            df.query(f"label=='{label}'")
+            .query(f'participant=="{participant}"')
+            .reset_index()
+        )
+        if len(all_axis_df) > 0:
+            fig, ax = plt.subplots()
+            all_axis_df[["acc_y", "acc_y", "acc_z"]].plot(ax=ax)
+            ax.set_ylabel("acc_y")
+            ax.set_xlabel("samples")
+            plt.title(f"{label} ({participant})".title())
+            plt.legend()
+
+for label in labels:
+    for participant in participants:
+        all_axis_df = (
+            df.query(f"label=='{label}'")
+            .query(f'participant=="{participant}"')
+            .reset_index()
+        )
+        if len(all_axis_df) > 0:
+            fig, ax = plt.subplots()
+            all_axis_df[["gyr_y", "gyr_y", "gyr_z"]].plot(ax=ax)
+            ax.set_ylabel("gyr_y")
+            ax.set_xlabel("samples")
+            plt.title(f"{label} ({participant})".title())
+            plt.legend()
 
 # --------------------------------------------------------------
 # Combine plots in one figure
 # --------------------------------------------------------------
 
+label = "row"
+participant = "A"
+combined_plot_df = (
+    df.query(f"label=='{label}'")
+    .query(f'participant=="{participant}"')
+    .reset_index(drop=True)
+)
+fig, ax = plt.subplots(nrows=2, sharex=True, figsize=(20, 10))
+combined_plot_df[["acc_y", "acc_y", "acc_z"]].plot(ax=ax[0])
+combined_plot_df[["gyr_y", "gyr_y", "gyr_z"]].plot(ax=ax[1])
+
+ax[0].legend(
+    loc="upper center", bbox_to_anchor=(0.5, 1.05), ncol=3, fancybox=True, shadow=True
+)
+ax[1].legend(
+    loc="upper center", bbox_to_anchor=(0.5, 1.05), ncol=3, fancybox=True, shadow=True
+)
+ax[1].set_xlabel("samples")
 
 # --------------------------------------------------------------
 # Loop over all combinations and export for both sensors
 # --------------------------------------------------------------
+
+labels = df["label"].unique()
+participants = df["participant"].unique()
+
+for label in labels:
+    for participant in participants:
+        combined_plot_df = (
+            df.query(f"label=='{label}'")
+            .query(f'participant=="{participant}"')
+            .reset_index()
+        )
+        
+        if len(combined_plot_df) > 0:
+            fig, ax = plt.subplots(nrows=2, sharex=True, figsize=(20, 10))
+            combined_plot_df[["acc_y", "acc_y", "acc_z"]].plot(ax=ax[0])
+            combined_plot_df[["gyr_y", "gyr_y", "gyr_z"]].plot(ax=ax[1])
+
+            ax[0].legend(
+                loc="upper center", bbox_to_anchor=(0.5, 1.05), ncol=3, fancybox=True, shadow=True
+            )
+            ax[1].legend(
+                loc="upper center", bbox_to_anchor=(0.5, 1.05), ncol=3, fancybox=True, shadow=True
+            )
+            ax[1].set_xlabel("samples")
+            plt.savefig(f'../../reports/figures/{label.title()} ({participant}).png')
+            plt.show()
